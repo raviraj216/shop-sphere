9fcd95be95cf23fefd5920dec74cd91299729560
devel9826 <mraviraj7@gmail.com>
2026-07-10 14:45:22 +0530

    updated routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -6,19 +6,20 @@ import { validate } from "../middleware/validate";
 import { createUserSchema ,updateUserSchema} from "../validators/user.validator";
 import {authorize} from "../middleware/authorize";
 
+import { authenticate } from "../middleware/auth.middleware";
+
 const router = Router();
 
 const controller = new UserController();
 
-router.post("/", validate(createUserSchema), controller.create);
+router.post("/",authenticate,authorize("admin"), validate(createUserSchema), controller.create);
  
+router.get("/",authenticate,authorize("admin"), controller.getAll);
 
-router.get("/", controller.getAll);
+router.get("/:id",authenticate, authorize("admin"),controller.getById);
 
-router.get("/:id", controller.getById);
+router.put("/:id", authenticate, validate(updateUserSchema), authorize("admin"), controller.update);
 
-router.put("/:id",validate(updateUserSchema), authorize("admin"), controller.update);
-
-router.delete("/:id", authorize("admin"), controller.delete);
+router.delete("/:id",authenticate, authorize("admin"), controller.delete);
 
 export default router;
